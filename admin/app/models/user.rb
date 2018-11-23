189675a5f9b8a54ea4db1504c1a3958797982dfd
佐々木優翔 <yuto.sasaki19@gmail.com>
2018-11-23 19:33:16 +0900

    user パスワードハッシュ化処理を追加

--- admin/app/models/user.rb
+++ admin/app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
--- admin/db/schema.rb
+++ admin/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_064512) do
+ActiveRecord::Schema.define(version: 2018_11_23_101654) do
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_064512) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
