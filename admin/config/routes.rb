c06a744dad43efb1cc94fa837f60545f6ebc18eb
佐々木優翔 <yuto.sasaki19@gmail.com>
2018-11-24 12:59:14 +0900

    ルーティングの修正

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  resources :microposts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :users
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  resources :users
+  resources :microposts
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
