0322bd64d1ccbd05c68dfb08ed67a0bc58f1be21
佐々木優翔 <yuto.sasaki19@gmail.com>
2018-11-24 15:52:32 +0900

    ユーザ登録 session処理の追加

--- admin/app/controllers/application_controller.rb
+++ admin/app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
-protect_from_forgery with: :exception
-
-  def hello
-    render html: "hello!"
-  end
+  protect_from_forgery with: :exception
+  include SessionsHelper
 
 end
