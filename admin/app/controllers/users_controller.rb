dae59899377338531e38606efc459f3e3f7bd66a
佐々木優翔 <yuto.sasaki19@gmail.com>
2018-11-25 16:51:43 +0900

    ログイン時処理の追加

--- admin/app/controllers/users_controller.rb
+++ admin/app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   # GET /users/1
@@ -19,6 +20,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
   end
 
   # POST /users
@@ -37,14 +39,12 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html {redirect_to @user, notice: 'User was successfully updated.'}
-        format.json {render :show, status: :ok, location: @user}
-      else
-        format.html {render :edit}
-        format.json {render json: @user.errors, status: :unprocessable_entity}
-      end
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールを更新しました。"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -68,5 +68,20 @@ class UsersController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- admin/app/models/user.rb
+++ admin/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   before_save { email.downcase! }
 
   validates :name, presence: true, length: {maximum: 50}
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
