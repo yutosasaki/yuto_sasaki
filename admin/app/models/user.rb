0f80c3d8afd850b50660a1793d117264d4662ce4
佐々木優翔 <yuto.sasaki19@gmail.com>
2018-11-25 10:07:51 +0900

    userid cookie保存処理を追加

--- admin/app/models/user.rb
+++ admin/app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_many :microposts
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
 
-  validates :name, presence: true
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: {maximum: 50}
+  validates :password, presence: true, length: {minimum: 6}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -12,4 +13,26 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
 
   has_secure_password
+
+  # 渡された文字列のハッシュ値を返す
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = self.new_token
+        update_attribute(:remember_digest, self.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
