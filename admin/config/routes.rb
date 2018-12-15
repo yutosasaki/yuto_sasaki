16e812289583d3f3b82ae7f4b1d358708f3879a2
佐々木優翔 <yuto.sasaki19@gmail.com>
2018-12-15 11:59:10 +0900

    アクティブユーザ、パスワードリセットのルーティング追加

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
   resources :users
@@ -11,4 +13,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
