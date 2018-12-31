b078ca436d82b6ec711b80b18a92e199b6bf388b
佐々木優翔 <yuto.sasaki19@gmail.com>
2018-12-31 13:10:57 +0900

    image_tag 動的生成処理追加

--- admin/app/helpers/users_helper.rb
+++ admin/app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+
+  IMAGE = 'test/images'.freeze
+  IMAGE_URL = 'test/images/u'.freeze
+  IMAGE_LEFT = 'image to left.'.freeze
+  IMAGE_RIGHT = 'image to right.'.freeze
+  ONLY_RIGHT = 'only right.'.freeze
+
+
+  def user_link
+    render partial: 'shared/custom_about'
+  end
+
+  def image_tags
+    image_left = [ IMAGE_LEFT, IMAGE_RIGHT ].sample(1)
+    @top_images = { image_left: image_left, only_right: ONLY_RIGHT, image_url: IMAGE_URL}
+  end
+
 end
