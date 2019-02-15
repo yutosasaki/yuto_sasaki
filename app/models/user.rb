7c97cf1e84d985f573ed265305965ed7d246baff
yutosasaki <yuto.sasaki19@gmail.com>
2019-02-15 10:31:01 +0900

    特異クラス　表記修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
 
   before_save { email.downcase! }
 
-  validates :name, presence: true, length: {maximum: 50}
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
@@ -14,21 +14,23 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  # 渡された文字列のハッシュ値を返す
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+  class << self
+    # 渡された文字列のハッシュ値を返す
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  # ランダムなトークンを返す
-  def self.new_token
-    SecureRandom.urlsafe_base64
+    # ランダムなトークンを返す
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   # 永続セッションのためにユーザーをデータベースに記憶する
   def remember
-    self.remember_token = self.new_token
-        update_attribute(:remember_digest, self.digest(remember_token))
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
